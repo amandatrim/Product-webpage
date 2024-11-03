241c9ff87ef27b59b00ad50e39926ccc64a3e2c6
amandatrim <amanda162@usf.edu>
2024-11-03 10:19:12 +0100

    Handle errors with .catch() and dsiplay error message

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ fetch('https://course-api.com/javascript-store-products')
     .then(data => {
         displayProducts(data); // Pass data to display function
     })
+    
     .catch(error => {
         document.getElementById('product-container').innerText = 'Failed to load products. Please try again later.';
         console.error('Fetch error:', error); // Log error to console
